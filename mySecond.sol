fcc7b7ab014faddfc76ec8d6bb3206d3857bae9e
haresh_polar <haresh.polar@minddeft.com>
2017-07-25 12:37:55 +0530

    sol

---
+++ mySecond.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.2;
+
+contract mySecond {
+
+  uint public message;
+  address public owner;
+  uint public data;
+  uint public datanew;
+
+  function mySecond() {
+      message = 2121;
+      data = message * 50 / 100;
+      owner = msg.sender;
+      datanew = message * 50 / 100;
+  }
+
+  function myCal() constant returns (uint) {
+      data = message * 50 / 100;
+      return data;
+  }
+  function myCalNew() constant returns (uint) {
+      datanew = message * 50 / 100;
+      return datanew;
+  }
+  function changeGreeting() constant returns(uint) {
+      return message;
+  }
+
+  function getBalance() constant returns (uint){
+      return owner.balance;
+  }
+
+  function getAddress() returns(address) {
+      return msg.sender;
+  }
+}
